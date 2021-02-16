1e7614f851e7af18c63615d198021b029c1c4bbe
0limpi0 <olymangue@gmail.com>
2021-02-16 13:36:59 +0200

    fix responsiveness on todoList

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -39,61 +39,13 @@ const TodoList = () => {
             <img src={cross} alt="cross" />
           </span>
         </aside>
-        <aside className="todo-item">
-          <span className="icon-item">
-            <img src={check} alt="check" />
-          </span>
-          <p className="item-text">
-            Complete online JavaScript course.
-        </p>
-          <span className="icon-item">
-            <img src={cross} alt="cross" />
-          </span>
+        <aside className="todo-item count-items">5 items left</aside>
+        <aside className="todo-item categories">
+          <span className="all">All</span>
+          <span className="active">Active</span>
+          <span className="completed">Completed</span>
         </aside>
-        <aside className="todo-item">
-          <span className="icon-item">
-            <img src={check} alt="check" />
-          </span>
-          <p className="item-text">
-            Complete online JavaScript course.
-        </p>
-          <span className="icon-item">
-            <img src={cross} alt="cross" />
-          </span>
-        </aside>
-        <aside className="todo-item">
-          <span className="icon-item">
-            <img src={check} alt="check" />
-          </span>
-          <p className="item-text">
-            Complete online JavaScript course.
-        </p>
-          <span className="icon-item">
-            <img src={cross} alt="cross" />
-          </span>
-        </aside>
-        <aside className="todo-item">
-          <span className="icon-item">
-            <img src={check} alt="check" />
-          </span>
-          <p className="item-text">
-            Complete online JavaScript course.
-        </p>
-          <span className="icon-item">
-            <img src={cross} alt="cross" />
-          </span>
-        </aside>
-        <aside className="todo-item">
-          <span className="icon-item">
-            <img src={check} alt="check" />
-          </span>
-          <p className="item-text">
-            Complete online JavaScript course.
-        </p>
-          <span className="icon-item">
-            <img src={cross} alt="cross" />
-          </span>
-        </aside>
+        <aside className="todo-item clear-completed">Clear completed</aside>
       </section>
     </main>
   );
